8c8c04aba9c00b3eebe96517f2cb93e57885640a
JB-Programming <jbjava00@gmail.com>
2021-02-20 22:22:59 -0600

    stuf

--- js/articleScript.js
+++ js/articleScript.js
@@ -1,11 +1,13 @@
 window.onload = function() {
     document.title = document.getElementById("article-title").innerHTML + " - The Cinema Center";
-    var starsE = document.getElementById("stars");
-	var stars = starsE.innerHTML.split(", ");
-	starsE.innerHTML = "";
-    for (var i = 0; i < stars.length; i++) {
-		var li = document.createElement("li");
-		li.innerHTML = stars[i];
-        starsE.appendChild(li);
-    }
+	if (document.getElementById("stars") != null) {
+		var starsE = document.getElementById("stars");
+		var stars = starsE.innerHTML.split(", ");
+		starsE.innerHTML = "";
+		for (var i = 0; i < stars.length; i++) {
+			var li = document.createElement("li");
+			li.innerHTML = stars[i];
+			starsE.appendChild(li);
+		}
+	}
 }
